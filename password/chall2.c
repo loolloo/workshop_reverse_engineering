261a582b5e1581116eb47b8a46b6a60218122a8c
Loocy <luc.anguilla@epitech.eu>
2021-02-02 17:56:01 +0100

    added azur's old challenge

--- password/chall2.c
+++ password/chall2.c
@@ -1,31 +1,42 @@
 //
-// chall1
+// chall2
 //
 
 #include <stdio.h>
 #include <string.h>
 #include <stddef.h>
+#include <stdlib.h>
 
-wchar_t password[] = L"passwd{If_your_password_is_too_short_just_make_it_wider!}";
-
-int cmp_wide_str(char *input)
+char *encodePassword(char *input)
 {
-    int i;
-    for (i = 0; password[i] == input[i] && input[i]; i++);
-    return password[i] - input[i];
+    char *encodedPasswd = malloc(sizeof(char) * strlen(input) + 1);
+    for (int i = 0; input[i] != '\0'; i++) {
+        encodedPasswd[i] = input[i] ^ 0x12;
+    }
+    encodedPasswd[strlen(input)] = '\0';
+    return (encodedPasswd);
+}
+
+void cmp_wide_str(char *input, char *encodedPassword)
+{
+    char *decodedPassword = encodePassword(encodedPassword);
+    for (int i = 0; decodedPassword[i]; i++) {
+        if (input[i] != decodedPassword[i]) {
+            printf("bark bark bark!\n");
+            return;
+        }
+    }
+    printf("You found it:%s\n", input);
+    return;
 }
 
 int main(int ac, char **av)
 {
+    char password[] = "bsaaevi_!``kQz`#af&ao"; //passwd{M3rryChr1st4s} clef hexa : 0x12
     if (ac != 2) {
-	puts("Usage: ./chall1 <password>");
-	return 1;
+        puts("Usage: ./chall1 <password>");
+        return 1;
     }
-
-    if (cmp_wide_str(av[1]) == 0)
-	puts("Success you can use this as the flag");
-    else
-	puts("Failed... Try again!");
-
+    cmp_wide_str(av[1], password);
     return 0;
 }
